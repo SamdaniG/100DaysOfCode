d4aada798e0c23e77b0ec98c6bf9c139e7426a5d
Ghulam Samdani <ghulam@uwindsor.ca>
2025-03-16 15:39:44 -0400

    First day added

--- Day1.py
+++ Day1.py
@@ -1,1 +1,6 @@
 #this is Day1 of 100DaysOfCode Challenge 
+#As embarassing simple this was it's necessary to do this
+print("Welcome to the Brand Name Generator")
+city=input("What's the name of the city you grew up in?\n")
+pet=input("What's your pet's name?\n")
+print(f"Your band name could be {city} {pet}")
