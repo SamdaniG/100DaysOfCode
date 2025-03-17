a5a6498a60b0bc83e5653367d2fafa3926f24db8
Ghulam Samdani <ghulam@uwindsor.ca>
2025-03-17 16:29:50 -0400

    Added Day3.py

---
+++ Day3.py
@@ -0,0 +1,49 @@
+#Day3 of 100DaysOfCode 
+print(r''' 
+  ____________________________________________________________________
+ / \-----     ---------  -----------     -------------- ------    ----\
+ \_/__________________________________________________________________/
+ |~ ~~ ~~~ ~ ~ ~~~ ~ _____.----------._ ~~~  ~~~~ ~~   ~~  ~~~~~ ~~~~|
+ |  _   ~~ ~~ __,---'_       "         `. ~~~ _,--.  ~~~~ __,---.  ~~|
+ | | \___ ~~ /      ( )   "          "   `-.,' (') \~~ ~ (  / _\ \~~ |
+ |  \    \__/_   __(( _)_      (    "   "     (_\_) \___~ `-.___,'  ~|
+ |~~ \     (  )_(__)_|( ))  "   ))          "   |    "  \ ~~ ~~~ _ ~~|
+ |  ~ \__ (( _( (  ))  ) _)    ((     \\//    " |   "    \_____,' | ~|
+ |~~ ~   \  ( ))(_)(_)_)|  "    ))    //\\ " __,---._  "  "   "  /~~~|
+ |    ~~~ |(_ _)| | |   |   "  (   "      ,-'~~~ ~~~ `-.   ___  /~ ~ |
+ | ~~     |  |  |   |   _,--- ,--. _  "  (~~  ~~~~  ~~~ ) /___\ \~~ ~|
+ |  ~ ~~ /   |      _,----._,'`--'\.`-._  `._~~_~__~_,-'  |H__|  \ ~~|
+ |~~    / "     _,-' / `\ ,' / _'  \`.---.._          __        " \~ |
+ | ~~~ / /   .-' , / ' _,'_  -  _ '- _`._ `.`-._    _/- `--.   " " \~|
+ |  ~ / / _-- `---,~.-' __   --  _,---.  `-._   _,-'- / ` \ \_   " |~|
+ | ~ | | -- _    /~/  `-_- _  _,' '  \ \_`-._,-'  / --   \  - \_   / |
+ |~~ | \ -      /~~| "     ,-'_ /-  `_ ._`._`-...._____...._,--'  /~~|
+ | ~~\  \_ /   /~~/    ___  `---  ---  - - ' ,--.     ___        |~ ~|
+ |~   \      ,'~~|  " (o o)   "         " " |~~~ \_,-' ~ `.     ,'~~ |
+ | ~~ ~|__,-'~~~~~\    \"/      "  "   "    /~ ~~   O ~ ~~`-.__/~ ~~~|
+ |~~~ ~~~  ~~~~~~~~`.______________________/ ~~~    |   ~~~ ~~ ~ ~~~~|
+ |____~jrei~__~_______~~_~____~~_____~~___~_~~___~\_|_/ ~_____~___~__|
+ / \----- ----- ------------  ------- ----- -------  --------  -------\
+ \_/__________________________________________________________________/
+
+
+''')
+print("Welcome to Treasure Island.\nYour mission is to find the treasure.")
+decision=input("You are at a crossroad, which way do you wanna go? Left or Right? ")
+if decision=="Right" or decision!="Left":
+    print("You fell into a hole.\nGAME OVER!!")
+else:
+    decision1=input("You arrive at a lake.\nWhat do you wanna do? Swim or Wait? ")
+    if decision1=="Swim" or decision1!="Wait":
+        print("Attacked by trout.\nGAME OVER!!")
+    else:
+        print("A boat picks you up and drops you at an island.")
+        decision2=input("You see a 3 coloured doors in front of you, Red, Blue and Yellow. Which door do you choose? ")
+        if decision2=="Red":
+            print("Burned by fire.\nGAME OVER!!")
+        elif decision2=="Blue":
+            print("Eaten by beasts.\nGAME OVER!!")
+        elif decision2=="Yellow":
+            print("The treasure is yours.\nYou win!")
+        else:
+            print("GAME OVER!!")
